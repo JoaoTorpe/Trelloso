1204b17e0f6cfd192d2416258da9a64be15be4bf
JoaoTorpe <victotorpe10@gmail.com>
2023-12-28 17:02:57 -0300

    Form and post methods

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
-console.log("main module!");
+const registerForm = document.querySelector("#registerForm")
+
+
+
+
+registerForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let newUser = {
+        name: document.getElementById("registerName").value ,
+        username: document.getElementById("registerUserName").value ,
+        avatar_url: document.getElementById("registerAvatar").value,
+        password: document.getElementById("registerPassword").value
+         
+    }
+    registerUser(newUser)
+
+
+})
+
+
+async function registerUser(data) {
+
+
+
+
+
+
+
+
+    try {
+        const response = await fetch("http://192.168.89.186:8087/api/v1/users/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+
+        const result = await response.json();
+        console.log("Success:", result);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+
+
+
+
+}
+
+
+
+
+
