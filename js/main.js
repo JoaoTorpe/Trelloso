23e2fc5c3c3a676e4ad5e0feb121e89ced0f7026
JoaoTorpe <victotorpe10@gmail.com>
2023-12-30 17:02:44 -0300

    login form and method

--- js/main.js
+++ js/main.js
@@ -1,52 +1,47 @@
+import login from "./auth.js"
+import { registerUser } from "./userService.js"
 const registerForm = document.querySelector("#registerForm")
+const loginForm = document.querySelector('#loginForm')
+const view = document.querySelector("#view")
 
 
-
-
+//Envio do form de registro
 registerForm.addEventListener("submit", (e) => {
     e.preventDefault()
+    console.log("Evento de envio do formulário disparado!");
     let newUser = {
         name: document.getElementById("registerName").value ,
         username: document.getElementById("registerUserName").value ,
         avatar_url: document.getElementById("registerAvatar").value,
-        password: document.getElementById("registerPassword").value
-         
+        password: document.getElementById("registerPassword").value  
     }
     registerUser(newUser)
+})
 
+
+//Envio do form de login
+loginForm.addEventListener("submit",(e)=>{
+  e.preventDefault()
+  let LoginFormData = new FormData(loginForm)  
+    login(LoginFormData)
+})
+
+
+//Visualizar a senha
+view.addEventListener("click",()=>{ 
+  const passwordInput = document.getElementById("loginPassword");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
 
 })
 
 
-async function registerUser(data) {
 
 
-
-
-
-
-
-
-    try {
-        const response = await fetch("http://192.168.89.186:8087/api/v1/users/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        });
-
-
-        const result = await response.json();
-        console.log("Success:", result);
-      } catch (error) {
-        console.error("Error:", error);
-      }
-
-
-
-
-}
 
 
 
